c4d6c2c948f0cffe8bddad4771d8a9d5eca92102
7520960-cloud <7520960@gmail.com>
2025-09-22 16:25:32 +0300

    Update App.swift

--- App.swift
+++ App.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct DaylineApp: App {
     var body: some Scene {
         WindowGroup {
-            AuthView() // стартовый экран (можешь поменять на нужный)
+            AuthView() // стартовый экран авторизации
         }
     }
 }

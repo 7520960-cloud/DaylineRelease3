05a2e6ca5cf46fc164561809330a5746e069d111
7520960-cloud <7520960@gmail.com>
2025-09-22 13:07:12 +0300

    Add files via upload

---
+++ Config/AppConfig.swift
@@ -0,0 +1,9 @@
+
+import Foundation
+enum AppConfig {
+    static let isAppStoreRelease = true
+    static let freeMaxGroups = 2
+    static let freeMaxAttachmentsPerMessage = 3
+    static let maxPollOptions = 10
+    static let minPasswordLength = 6
+}
